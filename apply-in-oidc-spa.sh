0255ed54ff043e2d4fa5374a48a70743b7762c34
garronej <joseph.garrone@protonmail.com>
2025-09-15 18:54:59 +0200

    Update update patch script

--- apply-in-oidc-spa.sh
+++ apply-in-oidc-spa.sh
@@ -6,8 +6,10 @@ rm -rf node_modules .yarn_home
 yarn
 rm -rf patches/oidc-client-ts+3.1.0.patch
 rm -rf node_modules/oidc-client-ts/dist/umd
+rm -rf node_modules/oidc-client-ts/dist/esm
 rm -rf node_modules/oidc-client-ts/dist/types
 cp -r ../oidc-client-ts/dist/umd   node_modules/oidc-client-ts/dist/
+cp -r ../oidc-client-ts/dist/esm   node_modules/oidc-client-ts/dist/
 cp -r ../oidc-client-ts/dist/types node_modules/oidc-client-ts/dist/
 npx patch-package oidc-client-ts
-yarn start-tanstack-router-file-based-example
+# yarn start-tanstack-router-file-based-example
